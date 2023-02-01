3d8c8e024929848f01470bb548e49be1d0f73e7f
rahulsolanki001 <43613414+rahulsolanki001@users.noreply.github.com>
2023-02-01 19:41:24 +0530

    first commit

---
+++ src/pages/countries/All.js
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import Search from "@/components/Search";
+import Filter from "@/components/Filter";
+import ShowCountries from "@/components/ShowCountries";
+import Link from "next/link";
+import {connect } from "react-redux";
+
+const All=({lightTheme})=>{
+    return(
+    <div className={lightTheme ? null : "dark"}>
+        <Navbar/>
+        <div id="search-filter">
+            <Search/>
+            <Filter/>
+        </div>
+        <ShowCountries/>
+        <Link href="/">Go Back</Link>
+    </div>
+    )
+}
+
+const mapStateToProps=(state)=>({
+    lightTheme:state.theme.lightTheme,
+})
+export default connect(mapStateToProps,{})(All);
